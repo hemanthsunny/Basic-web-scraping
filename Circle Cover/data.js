3954dd3a2a2c257007f9bfa4a44072fa4f0f1725
Naga Hemanth <hemanth.vja@gmail.com>
2019-02-03 20:50:17 +0530

    Data for the project

---
+++ Circle Cover/data.js
@@ -0,0 +1,14 @@
+exports.data = [
+	{tripType: "singletrip", location: "worldwideInclude", groupType: null, tripDays: 7, ages: [26]},
+	{tripType: "singletrip", location: "europe", groupType: null, tripDays: 7, ages: [20, 21]},
+	{tripType: "singletrip", location: "worldwideExclude", groupType: null, tripDays: 21, ages: [58, 56, 16]},
+	{tripType: "singletrip", location: "europe", groupType: null, tripDays: 7, ages: [55, 44, 32, 21]},
+	{tripType: "annualtrip", location: "europe", groupType: null, tripDays: 365, ages: [27]},
+	{tripType: "annualtrip", location: "worldwideInclude", groupType: null, tripDays: 365, ages: [27]},
+	{tripType: "annualtrip", location: "europe", groupType: null, tripDays: 365, ages: [25, 21]},
+	{tripType: "annualtrip", location: "worldwideExclude", groupType: null, tripDays: 365, ages: [11, 35, 55]},
+	{tripType: "annualtrip", location: "worldwideInclude", groupType: null, tripDays: 365, ages: [2, 25, 98]},
+	{tripType: "cruisetrip", location: "australia", groupType: null, tripDays: 7, ages: [54]},
+	{tripType: "cruisetrip", location: "europe", groupType: null, tripDays: 7, ages: [22, 26]},
+	{tripType: "cruisetrip", location: "worldwideInclude", groupType: null, tripDays: 7, ages: [25]}
+];
